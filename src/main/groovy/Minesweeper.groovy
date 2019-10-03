bf82a6cf58534cb4520e71da2c21fba98d2d791d
Natalie Sandford <nataliesandford@rentalcars.com>
2019-10-03 10:07:11 +0100

    Fixed failing tests

--- src/main/groovy/Minesweeper.groovy
+++ src/main/groovy/Minesweeper.groovy
@@ -9,20 +9,49 @@ class Minesweeper {
         def width = Integer.parseInt(gridDimensions[1])
         def height = Integer.parseInt(gridDimensions[0])
 
-        int[][] outputGrid = [height][width]
+        int[][] outputGrid = new int[height][width]
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
 
-                def isMine = rows[i+1].charAt(j) == ASTERISK
+                def isMine = rows[y+1].charAt(x) == ASTERISK
 
-
-
-                outputGrid[i][j] = "BALLOON"
+                if(isMine) {
+                    incrementNeighbours(outputGrid, width, height, x, y)
+                }
             }
         }
 
-        return "01*1\n0111\n0000\n0000"
+        def mergedMines = overlayGrids(rows, width, height, outputGrid)
+
+        return mergedMines
+    }
+
+    static def incrementNeighbours(int[][] outputGrid, int width, int height, int x, int y) {
+        for(int i = x - 1; i <= x + 1; i++) {
+            for(int j = y - 1; j <= y + 1; j++) {
+                if(i < 0 || i >= width) continue
+                if(j < 0 || j >= height) continue
+
+                outputGrid[j][i]++
+            }
+        }
+    }
+
+    static def overlayGrids(String[] rows, int width, int height, int[][] outputGrid) {
+        def result = ""
+
+            for(int j = 0 ; j < height; j++) {
+                for (int i = 0; i < width; i++) {
+                    result += rows[j + 1].charAt(i) == "*" ? "*" : outputGrid[j][i]
+                }
+
+                if (j < height - 1) {
+                    result += "\n"
+                }
+        }
+
+        return result
     }
 
 }
--- src/test/groovy/MinesweeperSpec.groovy
+++ src/test/groovy/MinesweeperSpec.groovy
@@ -2,7 +2,7 @@ import spock.lang.Specification
 
 class MinesweeperSpec extends Specification {
 
-    def "minesweeper can solve for field"() {
+    def "minesweeper can solve for a square field"() {
         given: "a field"
         def field = "4 4\n..*.\n....\n....\n...."
 
@@ -13,7 +13,7 @@ class MinesweeperSpec extends Specification {
         output == "01*1\n0111\n0000\n0000"
     }
 
-    def "minesweeper can solve for another field"() {
+    def "minesweeper can solve for a wide rectangle"() {
         given: "a field"
         def field = "3 5\n**...\n.....\n.*..."
 
@@ -22,5 +22,16 @@ class MinesweeperSpec extends Specification {
 
         then: "the out will be correct"
         output == "**100\n33200\n1*100"
+    }
+
+    def "minesweeper can solve for tall rectangle"() {
+        given: "a field"
+        def field = "4 2\n..\n.*\n..\n.."
+
+        when: "the field is parsed"
+        def output = Minesweeper.solveField(field)
+
+        then: "the out will be correct"
+        output == "11\n1*\n11\n00"
     }
 }
